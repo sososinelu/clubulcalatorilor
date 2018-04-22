0b0f2fe364192ff64fd905b2b6e980d129ff5fdd
Sorin Secan <sorinsoso4@gmail.com>
2018-04-22 20:28:52 +0100

    How it works block template

---
+++ modules/custom/clubulcalatorilor/src/Plugin/Block/HomepageHowItWorksBlock.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Provides the homepage how it works block
+ *
+ * @Block(
+ *   id = "homepage_how_it_works_block",
+ *   admin_label = @Translation("Homepage how it works block"),
+ * )
+ */
+
+namespace Drupal\clubulcalatorilor\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+
+class HomepageHowItWorksBlock extends BlockBase
+{
+  /**
+   * @return array
+   */
+  public function build()
+  {
+    if($node = \Drupal::routeMatch()->getParameter('node')) {
+
+      $sections = [];
+      $title = ($node->field_how_it_works_title ? $node->field_how_it_works_title->value : '');
+      $sections['section_1_title'] = ($node->field_hiw_section_1_title ? $node->field_hiw_section_1_title->value : '');
+      $sections['section_2_title'] = ($node->field_hiw_section_2_title ? $node->field_hiw_section_2_title->value : '');
+      $sections['section_3_title'] = ($node->field_hiw_section_3_title ? $node->field_hiw_section_3_title->value : '');
+      $sections['section_4_title'] = ($node->field_hiw_section_4_title ? $node->field_hiw_section_4_title->value : '');
+      $sections['section_1_text'] = ($node->field_hiw_section_1_text ? $node->field_hiw_section_1_text->value : '');
+      $sections['section_2_text'] = ($node->field_hiw_section_2_text ? $node->field_hiw_section_2_text->value : '');
+      $sections['section_3_text'] = ($node->field_hiw_section_3_text ? $node->field_hiw_section_3_text->value : '');
+      $sections['section_4_text'] = ($node->field_hiw_section_4_text ? $node->field_hiw_section_4_text->value : '');
+
+      return array(
+        '#theme' => 'homepage_how_it_works_template',
+        '#vars' => array(
+          'title' => $title,
+          'sections' => $sections,
+        ),
+        '#cache' => array('max-age' => 0),
+      );
+    }
+
+    return array();
+  }
+}
