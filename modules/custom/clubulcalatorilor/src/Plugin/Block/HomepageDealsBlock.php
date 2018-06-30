e3878c6d64340f8d9518ddf2ac3289bfb404146c
Sorin Secan <sorinsoso4@gmail.com>
2018-06-30 16:51:31 +0100

    Deal text section

--- modules/custom/clubulcalatorilor/src/Plugin/Block/HomepageDealsBlock.php
+++ modules/custom/clubulcalatorilor/src/Plugin/Block/HomepageDealsBlock.php
@@ -20,13 +20,13 @@ class HomepageDealsBlock extends BlockBase
   public function build()
   {
     if($node = \Drupal::routeMatch()->getParameter('node')) {
-      $deal_text = ($node->field_deal_text ? $node->field_deal_text->value : '');
+      $deals = ($node->field_deal_text ? $node->field_deal_text : '');
       $deal_image = ImageStyle::load('homepage_deal')->buildUrl($node->field_deal_image->entity->getFileUri());
 
       return array(
         '#theme' => 'homepage_deal_block_template',
         '#vars' => array(
-          'deal_text' => $deal_text,
+          'deals' => $deals,
           'deal_image' => $deal_image,
         ),
         '#cache' => array('max-age' => 0),
