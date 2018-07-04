cb8cd876283a584496db42edfa520cc33bb698e9
Sorin Secan <sorin.secan@nudgedigital.co.uk>
2018-07-04 11:05:21 +0100

    SendGrid integration custom module setup

---
+++ modules/custom/clubulcalatorilor_sendgrid/src/Controller/ClubulCalatorilorSendgridController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\clubulcalatorilor\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+
+class ClubulCalatorilorSendgridController extends ControllerBase
+{
+  /**
+   * ClubulCalatorilorSendgridController constructor.
+   */
+  public function __construct()
+  {
+
+  }
+}
