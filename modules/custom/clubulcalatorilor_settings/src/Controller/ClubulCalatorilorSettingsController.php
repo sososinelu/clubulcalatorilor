dd9290cc765d0e566a953b7e6a76514142b255f6
Sorin Secan <sorin.secan@nudgedigital.co.uk>
2018-07-04 11:02:46 +0100

    Refactored clubul calatorilor settings controller definition

---
+++ modules/custom/clubulcalatorilor_settings/src/Controller/ClubulCalatorilorSettingsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\clubulcalatorilor\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+
+class ClubulCalatorilorSettingsController extends ControllerBase
+{
+  /**
+   * ClubulCalatorilorSettingsController constructor.
+   */
+  public function __construct()
+  {
+
+  }
+}
